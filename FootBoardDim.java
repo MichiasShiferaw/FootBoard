85d0d09bf5de25ec0f19c22ab02df6d5e91287d8
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-16 11:21:23 -0500

    Create FootBoardDim.java

---
+++ FootBoardDim.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+
+public class FootBoardDim {
+  public static double footBoardDim(double width, double height) {
+    
+   // height = 144 / (width * length);
+    return height;
+  }
